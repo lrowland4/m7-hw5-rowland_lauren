ecd0b457e873534e5dca3d5d7745bfe5012bb0fe
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-10-04 18:42:01 -0400

    Add h1 ul and li

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ var pageBody = document.getElementsByTagName('body')[0];
 console.log(pageBody);
 var scriptTag = document.getElementsByTagName('script')[0];
 pageBody.insertBefore(main, scriptTag);
+
+var header = document.createElement('h1');
+main.appendChild(header);
+
+var ul = document.createElement('ul');
+header.appendChild(ul);
+
+var li = document.createElement('li');
+ul.appendChild(li);
+
+
